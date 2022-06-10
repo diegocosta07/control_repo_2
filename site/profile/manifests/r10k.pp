05ec5be1501ad8420ba1a0ae93dff71bd46fbd9b
Diego <diego.costa7711@gmail.com>
2022-06-10 14:14:52 -0400

    .

--- site/profile/manifests/r10k.pp
+++ site/profile/manifests/r10k.pp
@@ -1,6 +1,6 @@
 class profile::r10k {
   class {'r10k':
-    remote => 'https://github.com/diegocosta07/control_repo_2.git',
+    remote => 'https://github.com/diegocosta07/control_repo_2',
   }
   class {'r10k::webhook::config':
     use_mcollective => false,
